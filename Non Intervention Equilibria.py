90fbcb8b0f4fc4d6601cb26198de0a7c62d348ed
MaxMartin <maxmartin40@yahoo.com>
2026-04-19 17:12:35 -0600

    Add file for computing non-intervention equilibria completely numerically. This code is copied from a previous model using code composed by myself and Tyler Evans.

---
+++ Non Intervention Equilibria.py
@@ -0,0 +1,7 @@
+from sympy import *
+import numpy as np
+import random
+from scipy.optimize import root
+from scipy.optimize import fsolve, minimize
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
