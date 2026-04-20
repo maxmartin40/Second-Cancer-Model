4c8b0de800302ddbb3713afb25cf2416f6788ceb
MaxMartin <maxmartin40@yahoo.com>
2026-04-20 14:23:32 -0600

    Copy code and adjust for new system

--- Non Intervention Equilibria.py
+++ Non Intervention Equilibria.py
@@ -11,18 +11,43 @@ r=0.514; b=1.02*10**(-9); s_0=5000; k=10**(-4)
 #d_0=4.1; b_e=5.5; d_e=5.50001; k_e=1100; k_d=24000 #A and B in Figure 1
 d_0=5; b_e=6; d_e=4; k_e=1100; k_d=24000 #C and D in Figure 2
 
-def sys(t, z):
-    x, y = z
-    # dx = r*x*(1-b*x)-k*x*z
-    # dz = s_0 - d_0*z + (b_e*x*z)/(k_e+x) - (d_e*x*z)/(k_d+x)
-    return [r*x*(1-b*x)-k*x*z, s_0 - d_0*z + (b_e*x*z)/(k_e+x) - (d_e*x*z)/(k_d+x)]
+def F(z, k, d_e):
+    qty = s_0 - d_0*z + (b_e*(r-k*z)/(b*r)*z)/(k_e+(r-k*z)/(b*r)) - (d_e*(r-k*z)/(b*r)*z)/(k_d+(r-k*z)/(b*r))
+    return qty
 
-sol = solve_ivp(sys, [0,15], [10,5], dense_output=True)
+def find_equilibria_2(num_starts=25000, bounda=1000, boundb=5500):
+    # Initialize lists
+    message_list = []
+    d_e_y = []
+    k_x = []
+    # Generate random guesses inside range from above
+    for i in range(num_starts):
+        k = random.uniform(10**(-5), 10**(-2))
+        d_e = random.uniform(10**(-2), 1000)
+        guess = random.uniform(bounda,boundb)
+        # Run a solution to the system
+        sol = root(F, x0=guess, method='lm',options={'xtol': 1e-14,'ftol': 1e-14}, args=(k, d_e))
+        # Determine if there is a duplicate root and if not, add to root list
+        if sol.success==True:
+            r = sol.x
+            #print(np.shape(r))
+            #roots.append(r)
+            #roots.append([B,d,m])
+            message_list.append(sol.message)
+            k_x.append(k)
+            d_e_y.append(d_e)
+    return (k_x, d_e_y, message_list)
 
-t = np.linspace(0, 15, 300)
-z = sol.sol(t)
-plt.plot(t, z.T)
-plt.xlabel('t')
-plt.legend(['x','z'], shadow=True)
-plt.title('Lotka-Volterra System')
+equilibria = find_equilibria_2()
+
+# Plot solutions
+d_e_y = equilibria[1]
+k_x = equilibria[0]
+plt.plot(k_x,d_e_y, '.')
+plt.xlabel("k")
+plt.ylabel("d_e")
+#plt.xticks([0,10,20])
+#plt.yticks([0,0.0000063,0.0003,1])
+plt.title("Recreation of Figure 2(c)")
+plt.grid(True)
 plt.show()
