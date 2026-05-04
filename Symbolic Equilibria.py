abf01c17dff41234ebd1d060d9a41a6dea11cc40
MaxMartin <maxmartin40@yahoo.com>
2026-05-04 10:48:46 -0600

    Define symbols and equations

--- Symbolic Equilibria.py
+++ Symbolic Equilibria.py
@@ -1,5 +1,11 @@
 from sympy import roots, solve_poly_system
 from sympy import *
 
-r, x, b, k, z, s_0, d_0, b_e, k_e, d_e, k_d, lamb = symbols('r x b k z s_0 d_0 b_e k_e d_e k_d lamb')
+# Define symbols
+r, x, b, k, z, s_0, d_0, b_e, k_e, d_e, k_d, B, y, v, g, a, c, q, sigma, gamm, p, h = symbols('r x b k z s_0 d_0 b_e k_e d_e k_d B y v g a c q sigma gamm p h')
 
+# Define model 2.1 in Wei paper
+dxdt = r*x*(1-b*(x+y)) - (B*x*v)/(g+x) - k*x*z
+dydt = (B*x*v)/(g+x) - a*y - c*y*z
+dvdt = q*a*y - sigma*v - gamm*v*z
+dzdt = s_0 - d_0*z + (b_e*x*z)/(k_e+x) - (d_e*x*z)/(k_d+x) + (p*y*z)/(h+y)
